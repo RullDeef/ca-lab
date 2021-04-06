430316d951d784d13928aae826c114d5052917d0
Klimenko Alexey <klimenko0037@gmail.com>
2021-04-06 17:27:07 +0100

    done lab_4

--- lab_4/gui.py
+++ lab_4/gui.py
@@ -13,25 +13,32 @@ class DataTable(Sheet):
         super().__init__(master, data=self.__data, headers=["x", "y", "p"])
         self.sheet_display_dimensions(total_columns=3)
         self.enable_bindings()
-        self.extra_bindings([("end_edit_cell", self.__end_edit_cell)])
+        self.extra_bindings([("end_edit_cell", self.__execute_callbacks)])
         self.__callbacks = []
 
     def add_point(self, x, y, p=1):
         self.__data.append([x, y, p])
+        self.set_sheet_data(self.__data)
+        self.__execute_callbacks()
 
     def add_callback(self, callback):
         self.__callbacks.append(callback)
 
-    def __end_edit_cell(self, event):
+    def __execute_callbacks(self, event=None):
         for c in self.__callbacks:
             c()
-
-    def randomize_data(self, xmin, ymin, xmax, ymax, n=10):
+    
+    def initialize_data(self, n):
         self.__data.clear()
         for i in range(n):
-            x = round(xmin + random() * (xmax - xmin), 2)
-            y = round(ymin + random() * (ymax - ymin), 2)
-            self.__data.append([x, y, 1])
+            self.__data.append([0.0, 0.0, 1])
+        self.set_sheet_data(self.__data)
+
+    def randomize_data(self, xmin, ymin, xmax, ymax):
+        for row in self.__data:
+            row[0] = round(xmin + random() * (xmax - xmin), 2)
+            row[1] = round(ymin + random() * (ymax - ymin), 2)
+            row[2] = 1
         self.__data.sort(key=lambda d: d[0])
         self.set_sheet_data(self.__data)
 
@@ -61,12 +68,11 @@ class Plotter(tk.Frame):
             X = [row[0] for row in data]
             Y = [row[1] for row in data]
             self.__plot.plot(X, Y, "ro", label="данные")
-            self.__plot.legend()
-        if approx is not None:
-            colors = (None, "r", "g", "b", "y")
-            styles = (None, "-", "-", "-", "-")
-            for n, X, Y in approx:
-                self.__plot.plot(X, Y, colors[n] + styles[n], label=f"n = {n}")
+            if approx is not None:
+                colors = ("k", "r", "g", "b", "y")
+                styles = ("-", "-", "-", "-", "-")
+                for n, X, Y in approx:
+                    self.__plot.plot(X, Y, colors[n] + styles[n], label=f"n = {n}")
             self.__plot.legend()
         self.__canvas.draw()
 
@@ -78,19 +84,30 @@ class App(tk.Tk):
         self.__configure_grid()
 
         self.plotter = Plotter(self)
-        self.plotter.grid(row=0, column=0, sticky=tk.NSEW)
+        self.plotter.grid(row=0, column=0, columnspan=4, sticky=tk.NSEW)
 
         self.data_table = DataTable(self)
         self.data_table.add_callback(self.__display_data)
-        self.data_table.grid(row=0, column=1, rowspan=2, sticky=tk.NSEW)
+        self.data_table.grid(row=0, column=4, rowspan=2, sticky=tk.NSEW)
 
-        self.randomize_button = ttk.Button(self, text="случайные точки")
-        self.randomize_button.grid(row=1, column=0, padx=10, pady=10, sticky=tk.SE)
+        label = ttk.Label(self, text="N = ")
+        label.grid(row=1, column=0, padx=0, pady=10, sticky=tk.E)
+
+        self.n_value_string = tk.StringVar(self, value="10")
+        self.editbox = ttk.Entry(self, textvariable=self.n_value_string)
+        self.editbox.grid(row=1, column=1, padx=10, pady=10)
+
+        self.initialize_button = ttk.Button(self, text="инициализировать")
+        self.initialize_button.grid(row=1, column=2, padx=0, pady=10)
+        self.initialize_button.configure(command=self.initialize_action)
+
+        self.randomize_button = ttk.Button(self, text="случайное заполнение")
+        self.randomize_button.grid(row=1, column=3, padx=10, pady=10)
         self.randomize_button.configure(command=self.randomize_action)
 
     def __configure_grid(self):
-        rows = [14, 1]
-        cols = [1, 1]
+        rows = [1, 1]
+        cols = [1, 0, 0, 0, 1]
 
         for row, weight in enumerate(rows):
             self.rowconfigure(row, weight=weight)
@@ -110,7 +127,7 @@ class App(tk.Tk):
             X = [float(row[0]) for row in data]
             Y = [float(row[1]) for row in data]
             W = [float(row[2]) for row in data]
-            for n in 1, 2, 4:
+            for n in 0, 1, 2, 4:
                 func = self.approx_factory(X, Y, W, n)
                 X_ext = np.linspace(min(X), max(X), 200)
                 Y_ext = [func(x) for x in X_ext]
@@ -118,6 +135,13 @@ class App(tk.Tk):
 
         self.plotter.plot_data(data, approx)
 
+    def initialize_action(self):
+        try:
+            n = int(self.n_value_string.get())
+            self.data_table.initialize_data(n)
+        except Exception:
+            print("bad n")
+
     def randomize_action(self):
-        self.data_table.randomize_data(0, 0, 100, 100, n=6)
+        self.data_table.randomize_data(0, 0, 100, 100)
         self.__display_data()
--- lab_4/main.py
+++ lab_4/main.py
@@ -31,5 +31,50 @@ def best_sqr_approx(X: list, Y: list, W: list, n: int):
 
     return functor
 
+# Q6
+def best_sqr_approx_nm(X: list, Y: list, W: list, n: int):
+    def make_functor(A: list):
+        def functor(x):
+            y = 0
+            for i, a in enumerate(A):
+                y += a * x ** i
+            return y
+        return functor
+
+    def error(functor):
+        err = 0
+        for x, y, w in zip(X, Y, W):
+            err += w * (y - functor(x))**2
+        return err
+
+    N = len(X)
+    err_list = []
+
+    max_power = 10
+    for n in range(2, max_power):
+        for m in range(1, n):
+            Y_hat = array([[
+                sum(W[k] * Y[k] * X[k] ** i for k in range(N))
+                for i in (0, n, m)
+            ]])
+
+            mat = [[sum(W[k] * X[k] ** (i + j) for k in range(N))
+                    for j in (0, n, m)
+                ] for i in (0, n, m)
+            ]
+
+            mat = inv(mat)
+            errA = Y_hat.dot(mat).flatten()
+
+            A = [0] * (1 + max(n, m))
+            A[0], A[n], A[m] = errA
+
+            err = error(make_functor(A))
+            err_list.append((err, n, m, A))
+
+    best_err = sorted(err_list, key=lambda e: e[0])[0]
+    err, n, m, A = best_err
+    print(n, m)
+    return make_functor(A)
 
 App().feed_approximator(best_sqr_approx).mainloop()
